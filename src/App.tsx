d792a33e02d298db29596c8a8ef5a5e1fbe63872
Imran Nazir <imran.nazir@yahoo.co.uk>
2025-03-15 11:26:48 +0000

    docs: removed content from main page

--- src/App.tsx
+++ src/App.tsx
@@ -56,13 +56,6 @@ function App() {
           </div>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
       <button onClick={signOut}>Sign out</button>
     </main>
   );
